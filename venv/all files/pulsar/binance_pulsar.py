c40f13c204c76486e87f96dbd8c19660509046e1
Redmi <1993georgiy@gmail.com>
2023-07-25 00:04:01 +0300

    Реконнект к пульсару

--- venv/all files/pulsar/binance_pulsar.py
+++ venv/all files/pulsar/binance_pulsar.py
@@ -1,4 +1,3 @@
-# binance_pulsar.py
 from pulsar import Client
 import websocket
 import json
@@ -15,51 +14,55 @@ client = Client('pulsar://localhost:6650')
 producer = client.create_producer('binance')
 
 # Определение функций обратного вызова WebSocket
+trading_pairs = ['btcusdt', 'ethusdt', 'bnbusdt']  # и так далее для всех ваших торговых пар
 
 def on_open(ws):
     print('Соединение с WebSocket открыто.')
     # Подписка на все торговые пары
-    ws.send(json.dumps({
-        'method': 'SUBSCRIBE',
-        'params': ['!ticker@arr'],
-        'id': 1
-    }))
+    for pair in trading_pairs:
+        ws.send(json.dumps({
+            'method': 'SUBSCRIBE',
+            'params': [f'{pair}@depth'],
+            'id': 1
+        }))
 
 def on_message(ws, message):
-    # Convert message to list of Python dictionaries
-    data_list = json.loads(message)
+    # Convert message to Python dictionary
+    data = json.loads(message)
 
-    for data in data_list:
-        print(data)
-        if isinstance(data, dict):
-            # Keep only the necessary columns and rename them
-            filtered_data = {
-                'symbol': data['s'],
-                'askprice': data['a'],
-                'askqty': data['A'],
-                'bidprice': data['b'],
-                'bidqty': data['B']
-            }
+    # Check if the message is a depth update
+    if 'e' in data and data['e'] == 'depthUpdate':
+        # Check if there is enough market depth
+        if not data['b'] or not data['a']:
+            print(f'Not enough market depth: {data}')
+            return
 
-            # Print filtered data to console
-            # print(filtered_data)
+        # Keep only the necessary columns and rename them
+        filtered_data = {
+            'symbol': data['s'],
+            'askprice': data['a'][0][0],
+            'askqty': data['a'][0][1],
+            'bidprice': data['b'][0][0],
+            'bidqty': data['b'][0][1]
+        }
 
-            # Convert dictionary back to JSON string
-            filtered_message = json.dumps(filtered_data)
+        # Convert dictionary back to JSON string
+        filtered_message = json.dumps(filtered_data)
 
-            # Try to send the message
-            try:
-                # Convert message to bytes and publish to Pulsar
-                producer.send(bytes(filtered_message, 'utf-8'))
-            except:
-                print('Pulsar producer is closed. Attempting to reconnect.')
-                reconnect_pulsar()
+        # Try to send the message
+        try:
+            # Convert message to bytes and publish to Pulsar
+            producer.send(bytes(filtered_message, 'utf-8'))
+        except:
+            print('Pulsar producer is closed. Attempting to reconnect.')
+            reconnect_pulsar()
 
-            # Update the values in the database
-            df = pd.DataFrame([filtered_data])
-            engine = connect_to_db(database='crypto_intra')
-            update_or_insert_trade_pairs(engine, df)
-
+        # Update the values in the database
+        df = pd.DataFrame([filtered_data])
+        engine = connect_to_db(database='crypto_intra')
+        update_or_insert_trade_pairs(engine, df)
+    else:
+        print(f'Unexpected message: {data}')
 
 def on_error(ws, error):
     print(f'Ошибка WebSocket: {error}')
