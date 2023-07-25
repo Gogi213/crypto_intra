18c8f18c827935a10a22de005116ce98766ce64c
Redmi <1993georgiy@gmail.com>
2023-07-26 01:50:08 +0300

    Реконнект к пульсару

--- venv/all files/pulsar/binance_pulsar.py
+++ venv/all files/pulsar/binance_pulsar.py
@@ -1,97 +1,59 @@
-from pulsar import Client
 import websocket
+import threading
+import time
 import json
 import pandas as pd
-import sys
-sys.path.append('C:/Users/Redmi/PycharmProjects/crypto_intra/venv/all files')
 from bd_case import connect_to_db, update_or_insert_trade_pairs
+from pulsar import Client
 
+# List of currency pairs
+currency_pairs = ['ethusdt', 'btcusdt']
 
-# Инициализация клиента Pulsar
+# WebSocket connections dictionary
+ws_connections = {}
+
+# Data storage
+data = {pair: {} for pair in currency_pairs}
+
+# Pulsar client
 client = Client('pulsar://localhost:6650')
-
-# Инициализация продюсера Pulsar
 producer = client.create_producer('binance')
 
-# Определение функций обратного вызова WebSocket
-trading_pairs = ['btcusdt', 'ethusdt', 'bnbusdt']  # и так далее для всех ваших торговых пар
-
-def on_open(ws):
-    print('Соединение с WebSocket открыто.')
-    # Подписка на все торговые пары
-    for pair in trading_pairs:
-        ws.send(json.dumps({
-            'method': 'SUBSCRIBE',
-            'params': [f'{pair}@depth'],
-            'id': 1
-        }))
-
 def on_message(ws, message):
-    # Convert message to Python dictionary
-    data = json.loads(message)
+    # Parse the message
+    msg = json.loads(message)
 
-    # Check if the message is a depth update
-    if 'e' in data and data['e'] == 'depthUpdate':
-        # Check if there is enough market depth
-        if not data['b'] or not data['a']:
-            print(f'Not enough market depth: {data}')
-            return
+    # Update the data
+    global data
+    data[msg['s'].lower()] = {'symbol': msg['s'], 'askprice': msg['a'], 'askqty': msg['A'], 'bidprice': msg['b'], 'bidqty': msg['B']}
 
-        # Keep only the necessary columns and rename them
-        filtered_data = {
-            'symbol': data['s'],
-            'askprice': data['a'][0][0],
-            'askqty': data['a'][0][1],
-            'bidprice': data['b'][0][0],
-            'bidqty': data['b'][0][1]
-        }
+    # Update the values in the database
+    df = pd.DataFrame([data[msg['s'].lower()]])
+    print(df)
+    engine = connect_to_db(database='crypto_intra')
+    update_or_insert_trade_pairs(engine, df)
 
-        # Convert dictionary back to JSON string
-        filtered_message = json.dumps(filtered_data)
-
-        # Try to send the message
-        try:
-            # Convert message to bytes and publish to Pulsar
-            producer.send(bytes(filtered_message, 'utf-8'))
-        except:
-            print('Pulsar producer is closed. Attempting to reconnect.')
-            reconnect_pulsar()
-
-        # Update the values in the database
-        df = pd.DataFrame([filtered_data])
-        print(df)  # <--- Add this line to print the dataframe
-        engine = connect_to_db(database='crypto_intra')
-        update_or_insert_trade_pairs(engine, df)
-    else:
-        print(f'Unexpected message: {data}')
-
+    # Send the data to Pulsar
+    producer.send(bytes(json.dumps(data[msg['s'].lower()]), 'utf-8'))
 
 def on_error(ws, error):
-    print(f'Ошибка WebSocket: {error}')
+    print(error)
 
-def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
-    # Закрытие продюсера и клиента Pulsar
-    producer.close()
-    client.close()
+def on_close(ws):
+    print('WebSocket connection closed')
 
-# Connection management functions
+def on_open(ws):
+    print('WebSocket connection opened')
 
-def reconnect_pulsar():
-    global client, producer
-    try:
-        client.close()
-    except Exception as e:
-        print(f'Error closing Pulsar client: {e}')
-    client = Client('pulsar://localhost:6650')
-    producer = client.create_producer('binance')
+# Create a WebSocket connection for each currency pair
+for pair in currency_pairs:
+    ws = websocket.WebSocketApp(f'wss://stream.binance.com:9443/ws/{pair}@bookTicker',
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws_connections[pair] = ws
 
-# Подключение к WebSocket API Binance
-ws = websocket.WebSocketApp(
-    'wss://stream.binance.com:9443/ws',
-    on_open=on_open,
-    on_message=on_message,
-    on_error=on_error,
-    on_close=on_close
-)
-ws.run_forever()
+# Start all WebSocket connections
+for ws in ws_connections.values():
+    threading.Thread(target=ws.run_forever).start()
